87aefb571423130f0bf2470760b399d2a650e637
Víctor Oliva <olivarra1@gmail.com>
2023-08-21 23:21:42 +0200

    add interactivity

--- projection/src/App.svelte
+++ projection/src/App.svelte
@@ -2,35 +2,83 @@
   import { onMount } from "svelte";
   import { getPixel, imageLoaded } from "./source";
   import { mercator, normalize } from "./projection";
-  import { rotate } from "./rotation";
+  import { createRotation } from "./rotation";
 
   let canvasElement: HTMLCanvasElement;
 
   const projection = mercator();
 
-  async function draw() {
-    await imageLoaded;
+  let angle = Math.PI / 2.0;
 
-    const { width, height } = canvasElement;
-    const context = canvasElement.getContext("2d");
-    for (let y = 0; y < height; y++) {
-      for (let x = 0; x < width; x++) {
-        const destCoord = normalize({ x, y }, width, height);
-        let spherical = projection.toSpherical(destCoord);
-        spherical = rotate(spherical);
-        const { r, g, b } = getPixel(spherical);
-        context.fillStyle = "rgb(" + r + "," + g + "," + b + ")";
-        context.fillRect(x, y, 1, 1);
+  $: rotate = createRotation(angle);
+
+  let ready = false;
+  let isActive = false;
+
+  onMount(async () => {
+    await imageLoaded;
+    ready = true;
+  });
+
+  $: {
+    if (ready) {
+      if (isActive) {
+        canvasElement.width = 50;
+        canvasElement.height = 50;
+      } else {
+        canvasElement.width = 500;
+        canvasElement.height = 500;
       }
     }
   }
 
-  onMount(() => draw());
+  $: {
+    if (ready) {
+      const { width, height } = canvasElement;
+      const context = canvasElement.getContext("2d");
+      for (let y = 0; y < height; y++) {
+        for (let x = 0; x < width; x++) {
+          const destCoord = normalize({ x, y }, width, height);
+          let spherical = projection.toSpherical(destCoord);
+          spherical = rotate(spherical);
+          const { r, g, b } = getPixel(spherical);
+          context.fillStyle = "rgb(" + r + "," + g + "," + b + ")";
+          context.fillRect(x, y, 1, 1);
+        }
+      }
+    }
+  }
 </script>
 
 <main>
-  <canvas bind:this={canvasElement} width="500" height="500" />
+  <canvas
+    bind:this={canvasElement}
+    width="500"
+    height="500"
+    style="width: 500px; height: 500px;"
+  />
+  <div class="angle-input">
+    <input
+      type="range"
+      min={0}
+      max={2 * Math.PI}
+      step={0.01}
+      on:mousedown={() => (isActive = true)}
+      on:mouseup={() => (isActive = false)}
+      bind:value={angle}
+    />
+    <p>{((angle * 180) / Math.PI).toFixed(2)}º</p>
+  </div>
 </main>
 
 <style>
+  main {
+    display: flex;
+    flex-direction: column;
+  }
+  .angle-input {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
 </style>
--- projection/src/rotation.ts
+++ projection/src/rotation.ts
@@ -9,29 +9,29 @@ const kMatrix = [
   [-kVec[1], kVec[0], 0],
 ];
 
-const angle = Math.PI / 2.0;
-
-const rotationMatrix = add(
-  identity(3),
-  add(
-    multiply(kMatrix, Math.sin(angle)),
-    multiply(multiply(kMatrix, kMatrix), 1 - Math.cos(angle))
-  )
-);
-
-export function rotate({ phi, theta }: Spherical): Spherical {
-  const original_vec = [
-    [Math.cos(theta) * Math.cos(phi)],
-    [Math.sin(phi)],
-    [Math.sin(theta) * Math.cos(phi)],
-  ];
-  const rotated_vec = multiply(rotationMatrix, original_vec) as Matrix;
-
-  const resultPhi = Math.asin(rotated_vec.get([1, 0]));
-  const resultTheta = Math.atan2(
-    rotated_vec.get([2, 0]),
-    rotated_vec.get([0, 0])
+export function createRotation(angle: number) {
+  const rotationMatrix = add(
+    identity(3),
+    add(
+      multiply(kMatrix, Math.sin(angle)),
+      multiply(multiply(kMatrix, kMatrix), 1 - Math.cos(angle))
+    )
   );
 
-  return { phi: resultPhi, theta: resultTheta };
+  return function rotate({ phi, theta }: Spherical): Spherical {
+    const original_vec = [
+      [Math.cos(theta) * Math.cos(phi)],
+      [Math.sin(phi)],
+      [Math.sin(theta) * Math.cos(phi)],
+    ];
+    const rotated_vec = multiply(rotationMatrix, original_vec) as Matrix;
+
+    const resultPhi = Math.asin(rotated_vec.get([1, 0]));
+    const resultTheta = Math.atan2(
+      rotated_vec.get([2, 0]),
+      rotated_vec.get([0, 0])
+    );
+
+    return { phi: resultPhi, theta: resultTheta };
+  };
 }
